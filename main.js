ed19d9d4fdf9d72b4d234688f80a5a796d8dc2fa
080812 <74765678+080812@users.noreply.github.com>
2021-12-17 16:03:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+status=""
+objects=[]
+text=""
+
+function preload(){
+
+}
+
+function setup(){
+canvas=createCapture(400,400)
+canvas.center()
+video.hide()
+}
+
+function draw(){
+image(video,0,0,400,400)
+
+text=document.getElementById("TI").value
+
+if(status!=""){
+    objectdetector.detect(video,gotresult)
+
+    for(i=0;i<objects.length;i++){
+        document.getElementById("status").innerHTML="Status:Object Detected"
+        document.getElementById("no.object").innerHTML="Number of objects detected"+objects.length
+
+        fill("pink")
+        percentage=objects[i].confidence
+        confidence=percentage.toFixed(2)
+        text(objects[i].label+" "+confidence+"%",objects[i].x,objects[i].y)
+        noFill()
+        stroke("black")
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height)
+    }
+}
+}
+
+function start(){
+    objectdetector=ml5.objectDetector('COCOSSD',modelLoaded)
+}
+
+
+function modelLoaded(){
+console.log("model is loaded")
+status=true
+}
+
+function gotresult(error,results){
+    if(error){
+        console.log(error)
+    }
+    else{
+        console.log(results)
+        objects=results
+    }
+}
